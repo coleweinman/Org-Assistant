3541f57995621c45132b9d0a11fdb25cdcdf48f5
Emily Sturman <sturman@fb.com>
2022-08-17 14:13:09 -0700

    Style login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { Link, Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import HomePage from './pages/home/HomePage';
 import { AuthProvider } from './AuthProvider';
@@ -14,6 +12,7 @@ import OrgPage from './pages/org/OrgPage';
 import NavigationBar from './NavigationBar';
 import CheckInPage from './pages/checkIn/CheckInPage';
 import EventPage from './pages/events/EventPage';
+import "./stylesheets/App.scss";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBlMx0f35Ia49khVmeYFH6dmmpJEx2uMC0",
@@ -34,39 +33,37 @@ function App() {
 
   return (
     <AuthProvider auth={auth}>
-      {/* <NavigationBar> */}
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={
-              <AuthGuard>
-                <HomePage db={db} />
-              </AuthGuard>
-            } />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="orgs">
-              <Route 
-                path=":orgId" 
-                element={
-                  <AuthGuard>
-                    <OrgPage db={db} seasonId={"2022"} />
-                  </AuthGuard>
-                }
-              >
-              </Route>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={
+            <AuthGuard>
+              <HomePage db={db} />
+            </AuthGuard>
+          } />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="orgs">
+            <Route
+              path=":orgId"
+              element={
+                <AuthGuard>
+                  <OrgPage db={db} seasonId={"2022"} />
+                </AuthGuard>
+              }
+            >
             </Route>
-            <Route path="orgs/:orgId/checkin/:eventId"
-              element={
-                <CheckInPage db={db} />
-              }
-            />
-            <Route path="orgs/:orgId/events/:eventId"
-              element={
-                <EventPage db={db} />
-              }
-            />
           </Route>
-        </Routes>
-      {/* </NavigationBar> */}
+          <Route path="orgs/:orgId/checkin/:eventId"
+            element={
+              <CheckInPage db={db} />
+            }
+          />
+          <Route path="orgs/:orgId/events/:eventId"
+            element={
+              <EventPage db={db} />
+            }
+          />
+        </Route>
+      </Routes>
     </AuthProvider>
   );
 }
