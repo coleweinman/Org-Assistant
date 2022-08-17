2584021bfd8fca80c3d7c892a150955cfe2d2ea5
Emily Sturman <sturman@fb.com>
2022-08-17 12:22:41 -0700

    Improve backend logic
    - Add helper functions for error reporting & form validation
    - Fix error reporting/catching for AuthProvider sign in

--- src/helpers/Firebase.ts
+++ src/helpers/Firebase.ts
@@ -1,7 +1,6 @@
 import { AuthError, AuthErrorCodes } from "firebase/auth";
 
 export const getAuthErrorMessage = (e: AuthError): string => {
-  console.log(e.code);
   switch (e.code) {
     case AuthErrorCodes.EMAIL_EXISTS:
       return "The provided email is already linked to an account. Try a different email or log into your account.";
@@ -11,6 +10,8 @@ export const getAuthErrorMessage = (e: AuthError): string => {
       return "Invalid password. Please try again.";
     case AuthErrorCodes.USER_DELETED:
       return "Could not find an account with that email. Try creating an account instead.";
+    case AuthErrorCodes.NETWORK_REQUEST_FAILED:
+      return "Could not connect to the server. Please try again later.";
     default:
       return "Something went wrong. Please try again later.";
   }
