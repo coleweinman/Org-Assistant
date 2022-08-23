d40e0974913c44ee53ec7cea6b5c69dfd5935c99
Emily Sturman <emily@sturman.org>
2022-08-23 11:27:20 -0500

    Add styling for home page

--- src/pages/home/OrgCard.tsx
+++ src/pages/home/OrgCard.tsx
@@ -1,28 +1,19 @@
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Org } from "../../managers/OrgManager";
+import "../../stylesheets/OrgCard.scss";
 
-function OrgCard({ org }: { org: Org }) {
+type OrgCardProps = {
+	org: Org
+}
+
+const OrgCard: React.FC<OrgCardProps> = ({ org }) => {
+	const navigate = useNavigate();
+	const navigateToOrg = () => navigate(`orgs/${org.id}`);
 	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardActionArea component={Link} to={"orgs/" + org.id}>
-				{/* <CardMedia
-					component="img"
-					height="140"
-					image="/static/images/cards/contemplative-reptile.jpg"
-					alt="green iguana"
-				/> */}
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="div">
-						{org.name}
-					</Typography>
-					{/* <Typography variant="body2" color="text.secondary">
-						Lizards are a widespread group of squamate reptiles, with over 6,000
-						species, ranging across all continents except Antarctica
-					</Typography> */}
-				</CardContent>
-			</CardActionArea>
-		</Card>
+		<div className={"org-card"} onClick={navigateToOrg}>
+			<h3 className={"org-name"}>{org.name}</h3>
+		</div>
 	);
 }
 
