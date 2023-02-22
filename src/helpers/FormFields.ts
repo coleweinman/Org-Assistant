7bf6a4a38310605b06595c9b49099d5ffb79cada
Cole Weinman <coleweinman@gmail.com>
2023-02-22 16:42:56 -0600

    Hotfix for dropdown input type

--- src/helpers/FormFields.ts
+++ src/helpers/FormFields.ts
@@ -18,6 +18,6 @@ export const CheckInFields: FormField[] = [
   { id: "name", label: "Name", required: true, inputType: InputType.TEXT },
   { id: "email", label: "Email", required: true, inputType: InputType.EMAIL },
   { id: "schoolId", label: "UT EID", required: true, inputType: InputType.TEXT },
-  { id: "year", label: "Year", required: true, inputType: InputType.DROPDOWN, options: ["Freshman", "Sophmore", "Junior", "Senior", "Super Senior", "Grad Student"]},
+  { id: "year", label: "Year", required: true, inputType: InputType.DROPDOWN, options: ["Select", "Freshman", "Sophmore", "Junior", "Senior", "Super Senior", "Grad Student"]},
   { id: "discord", label: "Discord", required: false, inputType: InputType.TEXT },
 ];
