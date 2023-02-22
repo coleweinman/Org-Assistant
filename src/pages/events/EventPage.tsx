ce1dd655d04f2edf1e2ef6690cf2825f856b1787
Cole Weinman <coleweinman@gmail.com>
2023-02-22 16:43:08 -0600

    Pie chart

--- src/pages/events/EventPage.tsx
+++ src/pages/events/EventPage.tsx
@@ -9,13 +9,21 @@ import { CheckIn, getCheckIns } from "../../managers/CheckInManager";
 import { getEvent, OrgEvent } from "../../managers/EventManager";
 import loading from "../../images/loader.svg";
 import "../../stylesheets/EventPage.scss";
+import { ExportToCsv } from 'export-to-csv';
+import { Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
 interface EventPageProps {
   db: Firestore
 }
 
+interface YearGroup {
+  year: string,
+  quantity: number
+}
+
 const EventPage: React.FC<EventPageProps> = ({ db }) => {
   const [checkIns, setCheckIns] = React.useState<CheckIn[] | null>(null);
+  const [yearGroups, setYearGroups] = React.useState<YearGroup[]>([])
   const [event, setEvent] = React.useState<OrgEvent | null>(null);
 
   const { orgId, eventId } = useParams();
@@ -27,6 +35,19 @@ const EventPage: React.FC<EventPageProps> = ({ db }) => {
   React.useEffect(() => (
     getCheckIns(db, orgId!, eventId!, onCheckInsUpdate)
   ), [db, orgId, eventId]);
+
+  React.useEffect(() => {
+    let yearMap: Map<string, number> = new Map();
+    for (let checkIn of checkIns ?? []) {
+      if (!yearMap.has(checkIn.year)) {
+        yearMap.set(checkIn.year, 0);
+      }
+      yearMap.set(checkIn.year, yearMap.get(checkIn.year)! + 1);
+    }
+    let newYearGroups: YearGroup[] = [];
+    Array.from(yearMap.entries()).map((e) => newYearGroups.push({year: e[0], quantity: e[1]}));
+    setYearGroups(newYearGroups);
+  }, [checkIns]);
 
   // Listen and unsubscribe from event details
   React.useEffect(() => (
@@ -41,6 +62,14 @@ const EventPage: React.FC<EventPageProps> = ({ db }) => {
     );
   }
 
+  const copy = () => {
+    let clipboard: string = "Name\tUTEID\tEmail\tYear\tDiscord\tTimestamp";
+    for (let checkIn of checkIns ?? []) {
+      clipboard += `\n${checkIn.name}\t${checkIn.schoolId}\t${checkIn.email}\t${checkIn.year}\t${checkIn.discord}\t${checkIn.timestamp.toDate()}`;
+    }
+    navigator.clipboard.writeText(clipboard);
+  }
+
   return (
     <div className={"page event-page"}>
       <button className={"back-button"} onClick={() => navigate(-1)}>
@@ -52,7 +81,7 @@ const EventPage: React.FC<EventPageProps> = ({ db }) => {
         <button
           className={"view-check-in"}
           onClick={() => window.open(`/orgs/${orgId}/checkin/${eventId}`, "_blank")
-        }>
+          }>
           View check-in page
           <span className={"new-tab-icon"}>
             <FontAwesomeIcon icon={solid("arrow-up-right-from-square")} />
@@ -78,7 +107,21 @@ const EventPage: React.FC<EventPageProps> = ({ db }) => {
           </tbody>
         </table>
       </div>
+      <button onClick={copy}>COPY</button>
       <CheckInTable checkIns={checkIns} />
+        <PieChart width={400} height={400}>
+          <Pie
+            nameKey="year"
+            dataKey="quantity"
+            isAnimationActive={false}
+            data={yearGroups ?? []}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            label
+          />
+          <Tooltip />
+        </PieChart>
     </div>
   );
 }
